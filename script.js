8c91658daf0b67659ac14f2f4038d36517de6ac2
Kabita <63093391+kabita-18@users.noreply.github.com>
2023-06-05 13:36:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+
+const submitBtn = document.querySelector("#submitbutton");
+
+const formContainer = document.querySelector(".formcontainer")
+
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+const firstNameError = document.getElementById("firstnameerror");
+
+const lastNameError = document.getElementById("lastnameerror");
+
+const emailError = document.getElementById("emailerror");
+
+const passwordError = document.getElementById("passworderror");
+
+
+
+
+
+submitBtn.addEventListener('click',(e)=>{
+    if(firstName.value === ""){
+        firstNameError.style.display="block";
+        firstName.classList.add("error");
+        
+    }
+
+    
+    if(lastName.value === ""){
+        lastNameError.style.display="block";
+        lastName.classList.add("error");
+        
+        
+    }
+    if(email.value === ""){
+        emailError.style.display="block";
+        email.classList.add("error")
+    }
+
+    if(password.value === ""){
+        passwordError.style.display="block";
+        password.classList.add("error");
+        
+    }
+
+    
+/* make 4 variables, 
+    store input elemnts in it
+    check each input element
+        if value of element is empty
+            show the error message
+
+    if all input elements have some value, submit the form
+
+*/
+
+
+        
+})
